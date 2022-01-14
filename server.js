857b401b2c3a8c37607d370b422107b1e65fe371
rosausuario <96844148+rosausuario@users.noreply.github.com>
2022-01-14 16:23:57 -0600

    Add files via upload

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+
+class Server {
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT;
+        this.rutasUsuarios = "/usuarios";
+
+        this.middlewares();
+        this.routes();
+    }
+
+    middlewares() {
+        this.app.use(cors());
+    }
+
+    routes() {
+        this.app.use(this.rutasUsuarios, require("./routes/usuarios"));
+    }
+
+    listen() {
+        this.app.listen(this.port);
+    }
+}
+
+module.exports = Server;
